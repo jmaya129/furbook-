13a6b5e0ad2cc793247d8661812e4140c9fe52fa
jmaya129 <89950080+jmaya129@users.noreply.github.com>
2021-10-13 12:24:46 -0400

    classwork

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import './css/splash.css';
+import React from "react";
+import "./css/splash.css";
 
 function Splash() {
-  return(
-    <div className='Splash'>
-      <h1 className='primary text'>Furbook</h1>
-      <h5 className='secondary text'>Where pets aren't just man's best friend</h5>
+  return (
+    <div className="Splash">
+      <h1 className="primary text">Puffbook</h1>
+      <h5 className="secondary text">Who Needs Humans Anyways</h5>
+      <h5 className="quote text">
+        “Animals are such agreeable friends—they ask no questions; they pass no
+        criticisms.”
+      </h5>
     </div>
   );
 }
